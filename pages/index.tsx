9935f92c6b10b35610ffd01e00467c59aea36732
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2021-12-31 19:08:18 +0700

    layout done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Lightbox from '../components/Lightbox'
 import Main from '../components/Main'
 import NavBar from '../components/NavBar'
 
@@ -11,7 +12,8 @@ const Home: NextPage = () => {
       <hr />
       <NavBar />
       <Main />
-      
+      <div className="navbar-background"></div>
+      <Lightbox />
       <Footer />
     </>
   )
